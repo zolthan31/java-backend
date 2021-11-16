907cc9ecba48d0941a314a792b7c699f101aabed
romulo lima <dhb.tecnologia@gmail.com>
2021-11-16 20:31:21 -0300

    OrderStatus enum

--- src/main/java/com/dhbtecnologia/config/TestConfig.java
+++ src/main/java/com/dhbtecnologia/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.dhbtecnologia.domain.Order;
 import com.dhbtecnologia.domain.User;
+import com.dhbtecnologia.domain.enums.OrderStatus;
 import com.dhbtecnologia.repository.OrderRepository;
 import com.dhbtecnologia.repository.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Fulano Brown", "fulano:run@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Cicrano Green", "cicrano@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3 ));
